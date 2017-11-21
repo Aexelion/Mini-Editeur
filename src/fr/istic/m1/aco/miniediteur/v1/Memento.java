5fb134a9f3688f041078620d7b9561848c41fc49
Heartbroken-Git <corentin196@gmail.com>
2017-11-21 10:00:21 +0100

    doc ajoutées à Memento et deux de ses implémentations

--- src/fr/istic/m1/aco/miniediteur/v1/Memento.java
+++ src/fr/istic/m1/aco/miniediteur/v1/Memento.java
@@ -1,7 +1,14 @@
+/**
+ * @file Memento.java
+ * @author Dorian "Aexelion" DUMANGET
+ * @author Corentin "Heartbroken-Git" CHÉDOTAL
+ * @copyright LPRAB 1.0
+ */
+
 package fr.istic.m1.aco.miniediteur.v1;
 
 /**
- * Created by 16009566 on 13/10/17.
+ * @brief Interface servant à la réalisation du design pattern Memento
  */
 public interface Memento {
 }
--- src/fr/istic/m1/aco/miniediteur/v1/MementoInserer.java
+++ src/fr/istic/m1/aco/miniediteur/v1/MementoInserer.java
@@ -1,16 +1,32 @@
+/**
+ * @file MementoInserer.java
+ * @author Dorian "Aexelion" DUMANGET
+ * @author Corentin "Heartbroken-Git" CHÉDOTAL
+ * @copyright LPRAB 1.0
+ */
+
 package fr.istic.m1.aco.miniediteur.v1;
 
 /**
- * Created by 16009566 on 13/10/17.
+ * @brief Classe implémentant le Memento appliqué à l'action Inserer
+ * @details Implémentation jouant donc le rôle de "concrete memento" du design pattern Memento
  */
 public class MementoInserer implements Memento {
 
     private String text;
 
+	/**
+	 * @brief Constructeur du MementoInserer destiné à sauvegarder un texte inséré
+	 * @param str un String du texte inséré à conserver
+	 */
     public MementoInserer (String str){
         text = str;
     }
 
+	/**
+	 * @brief Accesseur du texte sauvegardé
+	 * @return Le texte originellement inséré, sous forme de String
+	 */
     public String getText(){
         return this.text;
     }
