36c8fb55bfb9d98b3d89aff6e709a44ddb328502
Aexelion <aexelion@gmail.com>
2017-11-30 20:00:30 +0100

    Tests Buffer, PressePapier, Selection fini

---
+++ test/fr/istic/m1/aco/miniediteur/v1/PressePapierTest.java
@@ -0,0 +1,56 @@
+package fr.istic.m1.aco.miniediteur.v1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 16009566 on 30/11/17.
+ */
+public class PressePapierTest {
+    PressePapier pp;
+    @Before
+    public void init() {
+        pp = new PressePapier("");
+    }
+    @Test
+    public void getTxt() throws Exception {
+        String str = pp.getTxt();
+        assertTrue(str.equals(""));
+
+        pp = new PressePapier("123456");
+        str = pp.getTxt();
+        assertTrue(str.equals("123456"));
+    }
+
+    @Test
+    public void setTxt() throws Exception {
+        String str;
+
+        pp.setTxt("");
+        str = pp.getTxt();
+        assertTrue(str.equals(""));
+
+        pp.setTxt("123");
+        str = pp.getTxt();
+        assertTrue(str.equals("123"));
+
+        pp.setTxt("456");
+        str = pp.getTxt();
+        assertTrue(str.equals("456"));
+    }
+
+    @Test
+    public void Testclone() throws Exception {
+        PressePapier clonePP = pp.clone();
+        assertTrue(pp != clonePP);
+        assertTrue(pp.getTxt().equals(clonePP.getTxt()));
+
+        pp = new PressePapier("123456");
+        clonePP = pp.clone();
+        assertTrue(pp != clonePP);
+        assertTrue(pp.getTxt().equals(clonePP.getTxt()));
+    }
+
+}
